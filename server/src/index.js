b1c18e8931dd20f80ebd7ff6205f25f4bdfbd454
ankerbachryhl <ankerbachryhl@gmail.com>
2018-04-03 14:59:18 +0200

    Updated link again to prod server

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://eu1.prisma.sh/public-cookietongue-807/texty/dev', // the endpoint of the Prisma DB service
+      endpoint: 'https://eu1.prisma.sh/public-cookietongue-807/texty/', // the endpoint of the Prisma DB service
       secret: 'mysecret123', // specified in database/prisma.yml
       debug: true, // log all GraphQL queryies & mutations
     }),
